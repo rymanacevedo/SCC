bdd8293aea9425203f8a1966432f769d58614570
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-27 00:02:06 +0000

    updated deployment config

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,5 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [reactRouter(), tailwindcss()],
 });
