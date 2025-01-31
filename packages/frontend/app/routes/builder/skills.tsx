5689797610454b482434313fff2bcfac8d1cbc22
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-01-31 23:04:16 +0000

    adjust button formatting

--- packages/frontend/app/routes/builder/skills.tsx
+++ packages/frontend/app/routes/builder/skills.tsx
@@ -66,7 +66,7 @@ export default function Skills() {
           <fetcher.Form
             action="/api/skills"
             method="post"
-            className="flex justify-around mb-6"
+            className="inline-flex gap-2 w-full"
           >
             {/* TODO: adjust the size */}
             <Input
@@ -74,6 +74,12 @@ export default function Skills() {
               label="Search by Job Title for Pre-Written Examples"
               id="jobSearch"
             />
+            {/* <div className="absolute block  top-35 bg-white text-black">
+              <li className=''>Cashier</li>
+              <li>Truck Driver</li>
+              <li>Construction Worker</li>
+              <li>Metal Sheetworker</li>
+            </div> */}
             {/* <div className="absolute inset-y-0 left-0 pl-3 flex items-center">
                 <svg
                   role="img"
@@ -98,12 +104,30 @@ export default function Skills() {
                     className="flex-1 border shadow-sm pl-10 pr-4 py-2 rounded-md"
                     placeholder="e.g., Software Engineer, Project Manager"
                   /> */}
-            <Button
-              textSize="text-xs"
-              size="small"
-              text={fetcher.state !== 'idle' ? 'Searching...' : 'Search'}
-              action="submit"
-            />
+            <div className="flex items-center">
+              <Button
+                textSize="text-xs"
+                type="icon"
+                text="Search"
+                icon={
+                  <svg
+                    aria-hidden="true"
+                    className="h-5 w-5"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                    />
+                  </svg>
+                }
+                action="submit"
+              />
+            </div>
           </fetcher.Form>
 
           {/* Example Skills Based on Search */}
@@ -248,10 +272,10 @@ export default function Skills() {
               </div>
             </div>
 
-            {/* Hidden inputs to submit all skills */}
+            {/* Hidden inputs to submit all skills
             {userSkills.map((skill) => (
               <input key={skill} type="hidden" name="skills" value={skill} />
-            ))}
+            ))} */}
 
             {/* Navigation Buttons */}
             <div className="flex justify-between pt-4">
