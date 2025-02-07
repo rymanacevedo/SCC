8cc6a5504119efb376042e255fd58221e0cd026a
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-02-07 21:19:34 +0000

    turned textarea into controled component so react can keep track of deletion

--- packages/frontend/app/routes/builder/experienceEntry.tsx
+++ packages/frontend/app/routes/builder/experienceEntry.tsx
@@ -124,6 +124,7 @@ export default function ExperienceEntry() {
             />
             <div className="space-y-2 border rounded-md p-4">
               {/* This would be populated based on search results */}
+              {/* TODO: add a loading state */}
               <Heading
                 level="h3"
                 text="Recommended Experience"
@@ -241,11 +242,11 @@ export default function ExperienceEntry() {
                           viewBox="0 0 460.775 460.775"
                         >
                           <title>{`Remove ${experience}`}</title>
-                          <g id="SVGRepo_bgCarrier" stroke-width="0" />
+                          <g id="SVGRepo_bgCarrier" strokeWidth="0" />
                           <g
                             id="SVGRepo_tracerCarrier"
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
                           />
                           <g id="SVGRepo_iconCarrier">
                             <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55 c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55 c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505 c-6.074,6.077-6.074,15.911,0,21.986l32.709,32.719c2.911,2.911,6.865,4.55,10.992,4.55c4.127,0,8.08-1.639,10.994-4.55 l171.117-171.12l171.118,171.12c2.913,2.911,6.866,4.55,10.993,4.55c4.128,0,8.081-1.639,10.992-4.55l32.709-32.719 c6.074-6.075,6.074-15.909,0-21.986L285.08,230.397z" />
@@ -260,8 +261,12 @@ export default function ExperienceEntry() {
                     <textarea
                       className="w-full border-0 field-sizing-content"
                       wrap="soft"
-                      id={`experience-${index}`}
-                      defaultValue={experience}
+                      value={experience}
+                      onChange={(e) => {
+                        const newExperiences = [...userExperience];
+                        newExperiences[index] = e.target.value;
+                        setUserExperience(newExperiences);
+                      }}
                     />
                   </div>
                 ))}
