8f6a27a949bf05a51b4219bf8a5fefe60be6a8ca
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-02-12 18:56:28 +0000

    adjusted styling for work summaries

--- packages/frontend/app/routes/builder/experienceSummary.tsx
+++ packages/frontend/app/routes/builder/experienceSummary.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react';
 import Heading from '../../components/Heading';
 import { getRequiredUserTrait } from '../../utils/user';
 import { NavLink, useLoaderData } from 'react-router';
+import Button from '../../components/Button';
 
 export async function clientLoader() {
   const experiences = getRequiredUserTrait('experience');
@@ -12,22 +13,33 @@ function ExperienceSummary() {
   return (
     <main className="max-w-6xl mx-auto">
       <Heading text="Work history summary" level="h1" size="text-3xl" />
-      {ex.map((e) => (
-        <div key={e.jobId}>
-          <Heading level="h2" size="text-2xl" text={e.jobTitle} />
-          <em>{e.location}</em> |
-          <em>
-            {e.startDate} - {e.endDate}
-          </em>
-          <ul className="p-4 list-disc">
-            {e.details?.map((d, index) => (
-              <li key={`${e.jobId}-detail-${index}`}>{d}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+      <div className="space-y-4">
+        {ex.map((e, index) => (
+          <div
+            className="border pr-10 pl-14 pt-3 pb-3 rounded-md  cursor-pointer 
+          hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors relative"
+            key={e.jobId}
+          >
+            <div className="">
+              <span className="absolute left-0 top-0 border border-l-0 border-t-0 pt-2 pb-2 pr-4 pl-4 rounded">
+                {index + 1}
+              </span>
+              <Heading level="h2" size="text-2xl" text={e.jobTitle} />
+            </div>
+            <em>{e.location}</em> |
+            <em>
+              {e.startDate} - {e.endDate}
+            </em>
+            <ul className="p-4 list-disc">
+              {e.details?.map((d, index) => (
+                <li key={`${e.jobId}-detail-${index}`}>{d}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
 
-      <NavLink to="/experience">Add another position</NavLink>
+      <NavLink to="/experience">+ Add another position</NavLink>
     </main>
   );
 }
