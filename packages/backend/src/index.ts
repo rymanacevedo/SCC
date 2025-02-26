2b0bd11c9193bfe4807daf79df395da8bfaaf284
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-02-26 21:02:53 +0000

    integrated frontend ai services for summary, need to fix cors issues, but able to ping backend server

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,43 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+import { z } from 'zod';
+import { zValidator } from '@hono/zod-validator';
+import { createSummaries } from './services/ai';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(cors());
 
-export default app
+const schema = z.object({
+  prompt: z.string().min(1),
+});
+
+app.post(
+  '/api/generate',
+  zValidator('json', schema),
+
+  async (c) => {
+    try {
+      const apiKey = process.env.GEMINI_API_KEY;
+      console.log(apiKey)
+      if (!apiKey) {
+        console.error('GEMINI_API_KEY not set in environment variables');
+        return c.json({ error: 'GEMINI_API_KEY not set' }, 500);
+      }
+
+      const { prompt } = c.req.valid('json');
+
+      const response = await createSummaries(prompt);
+
+      return c.json(response);
+    } catch (error) {
+      console.error('Error generating content:', error);
+      return c.json(
+        { error: 'Failed to generate content', details: error },
+        500,
+      );
+    }
+  },
+);
+
+export default app;
