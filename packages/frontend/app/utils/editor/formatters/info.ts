812af595dd9eeff08fb078c9dac9c827d2bbd369
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-04-09 20:23:41 +0000

    removed location from resume

--- packages/frontend/app/utils/editor/formatters/info.ts
+++ packages/frontend/app/utils/editor/formatters/info.ts
@@ -2,9 +2,7 @@ import type { User } from './../../user';
 type Info = User['info'];
 export function formatInfoString(info: Info) {
   const location = [info.city, info.state].filter(Boolean).join(', ');
-  const components = [info.email, info.phone, location]
-    .filter(Boolean)
-    .join(' | ');
+  const components = [info.email, info.phone].filter(Boolean).join(' | ');
 
   return components;
 }
